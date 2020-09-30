b9ab8fa8d4094c93bb7215761cec899df40eaa45
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-30 20:37:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,4 @@ class Song
   
   attr_accessor :title, :artist
   
-  # def initialize
-  #   @title = Artist.new
-  # end
-  
 end
-
-# drake = Artist.new
-# hotline_bling = Song.new
- 
-# hotline_bling.artist = drake
